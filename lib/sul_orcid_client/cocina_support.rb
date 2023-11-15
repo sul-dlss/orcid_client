c922c945f8e94bd2e39562fbeca396723c774281
Peter Mangiafico <peter@mangiafico.org>
2023-11-15 12:22:24 -0800

    deal with alternate orcid formats for contributors

--- lib/sul_orcid_client/cocina_support.rb
+++ lib/sul_orcid_client/cocina_support.rb
@@ -17,6 +17,13 @@ class SulOrcidClient
       identifier = contributor.identifier.find { |identifier| identifier.type == "ORCID" }
       return unless identifier
 
+      # some records have the full ORCID URI in the data, just return it if so, e.g. druid:gf852zt8324
+      return identifier.uri if identifier.uri
+      return identifier.value if identifier.value.start_with?("https://orcid.org/")
+
+      # some records have just the ORCIDID without the URL prefix, add it if so, e.g. druid:tp865ng1792
+      return URI.join("https://orcid.org/", identifier.value).to_s if identifier.source.uri.blank?
+
       URI.join(identifier.source.uri, identifier.value).to_s
     end
 
